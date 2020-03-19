82e00ea06b7c396741e71bfdb2a1c89635107a21
wow1548 <wow1548@naver.com>
2020-03-19 11:46:26 +0900

    Create thunbmail, time field in Video model

--- app/video/models.py
+++ app/video/models.py
@@ -3,6 +3,32 @@ from django.contrib.auth.models import User
 from django.conf import settings
 from django.utils.text import slugify
 
+import cv2
+
+
+def get_video_data(filepath):
+    video = cv2.VideoCapture(filepath)
+
+    frame_count = video.get(cv2.CAP_PROP_FRAME_COUNT)
+    fps = video.get(cv2.CAP_PROP_FPS)
+
+    video.set(cv2.CAP_PROP_POS_FRAMES, int(frame_count/2))
+
+    _, frame = video.read()
+
+    cv2.imwrite(filepath+"-thumbnail.png", frame)
+
+    video.release()
+
+    duration = frame_count/fps
+
+    minutes = int(duration/60)
+    seconds = int(duration % 60)
+
+    time = f'{minutes}:{seconds}'
+
+    return time
+
 
 class Video(models.Model):
     author = models.ForeignKey(
@@ -12,13 +38,18 @@ class Video(models.Model):
     video_link = models.FilePathField(path=settings.MEDIA_ROOT)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    slug = models.SlugField(max_length=50)
+    thumbnail = models.ImageField(blank=True)
+    time = models.CharField(max_length=12, blank=True)
+    slug = models.SlugField(max_length=50, blank=True)
 
     def __str__(self):
         return self.title
 
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
         self.slug = slugify(self.title, allow_unicode=True)
+        self.time = get_video_data(self.video_link)
+        self.video_link = "/"+"/".join(self.video_link.split("/")[2:])
+        self.thumbnail = f"{self.video_link}-thumbnail.png"
 
         return super().save(force_insert=force_insert, force_update=force_update, using=using, update_fields=update_fields)
 
