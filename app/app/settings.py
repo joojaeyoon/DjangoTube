1f34957d6d9207086ef2c4338153ab588a9ad2c6
wow1548 <wow1548@naver.com>
2020-03-19 09:54:59 +0900

    Added travis

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
     'video'
 ]
 
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
